cf4cec8890e0dd29ca03d2ebefbe11931754a37c
Oleksandr Utkin <neoleksandrutkin@gmail.com>
2020-12-03 15:44:39 +0200

    add input focus

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,15 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import './SearchForm.scss';
 import {connect} from 'react-redux';
 import fetchRepos from "../../store/actions/getRepos";
 
 const SearchForm = (props) => {
     const [inputValue, setInputValue] = useState('');
+    const inputRef = useRef();
+
+    useEffect(() => {
+        inputRef.current.focus();
+    });
 
     const handleChange = (e) => {
         setInputValue(e.target.value)
@@ -23,6 +28,7 @@ const SearchForm = (props) => {
                 className='search-form__input'
                 value={inputValue}
                 onChange={handleChange}
+                ref={inputRef}
             />
             <button type='submit' className='search-form__submit-btn'>submit</button>
         </form>
